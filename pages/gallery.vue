e2755d323733dd2b7447bda9480a21be8b9bf380
Paul Lindquist <paul.lindquist@gmail.com>
2023-11-21 09:03:26 -0600

    Add photos of church

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -3,6 +3,43 @@
 <template>
   <div class="container">
     <h1>Gallery</h1>
+    <h2>Christ Ministries Church</h2>
+    <div class="masonry">
+      <div class="item">
+        <img src="/img/gallery/church_9.jpg" />
+      </div>
+      <div class="item">
+        <img src="/img/gallery/church_10.jpg" />
+      </div>
+      <div class="item">
+        <img src="/img/gallery/church_11.jpg" />
+      </div>
+      <div class="item">
+        <img src="/img/gallery/church_1.jpg" />
+      </div>
+      <div class="item">
+        <img src="/img/gallery/church_2.jpg" />
+      </div>
+      <div class="item">
+        <img src="/img/gallery/church_3.jpg" />
+      </div>
+      <div class="item">
+        <img src="/img/gallery/church_4.jpg" />
+      </div>
+      <div class="item">
+        <img src="/img/gallery/church_5.jpg" />
+      </div>
+      <div class="item">
+        <img src="/img/gallery/church_6.jpg" />
+      </div>
+      <div class="item">
+        <img src="/img/gallery/church_7.jpg" />
+      </div>
+      <div class="item">
+        <img src="/img/gallery/church_8.jpg" />
+      </div>
+    </div>
+    <h2>Residential Work</h2>
     <div class="masonry">
       <div class="item">
         <img src="/img/gallery/one.jpg" />
@@ -26,10 +63,15 @@
   </div>
 </template>
 <style scoped>
-h1 {
+h1,
+h2 {
+  font-size: 2rem;
   text-align: center;
   font-weight: 700;
   padding: 1.5rem;
+}
+h2 {
+  font-size: 1.5rem;
 }
 .container {
   margin: 2rem auto;
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,16 @@
 <template>
-  <ServiceSection />
-  <AboutUsSection />
-  <ProjectsSection />
-  <TestimonialsSection />
-  <WhyChoose />
-  <ContactUsForm />
+  <div class="desktop-container">
+    <ServiceSection />
+    <AboutUsSection />
+    <ProjectsSection />
+    <TestimonialsSection />
+    <WhyChoose />
+    <ContactUsForm />
+  </div>
 </template>
+<style scoped>
+.desktop-container {
+  max-width: 992px;
+  margin: 0 auto;
+}
+</style>
